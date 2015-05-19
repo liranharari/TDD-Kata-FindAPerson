e53e70dcb6ab58ac1635dc7f688f719bd6eca38a
liranharari <liran.94.h@gmail.com>
2015-05-19 13:57:16 +0300

    GREEN: test 3 pass

--- findPerson.py
+++ findPerson.py
@@ -10,8 +10,17 @@ class FindProjTests(unittest.TestCase):
 		self.assertEquals([],posts)
 	
 	def test_get_all_posts_for_name(self):
+		posts = self.crowdmap.get_all_posts_for("Or")
 		self.assertEquals(["I met Or A. at Chabad house Bangkok","We found Or A. R.I.P at Langtang valley"],posts)
 		
+	def test_return_boolean_if_location_in_the_post(self):
+		location_exist = self.crowdmap.is_location_for_name("Or")
+		self.assertTrue(location_exist) 
+		
+	def test_check_different_location(self):
+		different_location = self.crowdmap.is_different_location_for_name("Or")
+		self.assertTrue(different_location)
+	
 		
 if __name__ == '__main__':
 	unittest.main()
