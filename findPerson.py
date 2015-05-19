202895a6d62cf834644b9fa36aa13a7a5f8ae5b5
liranharari <liran.94.h@gmail.com>
2015-05-19 13:50:37 +0300

    RED: test 2

--- findPerson.py
+++ findPerson.py
@@ -8,6 +8,10 @@ class FindProjTests(unittest.TestCase):
 	def test_get_all_posts_for_missing_name(self):
 		posts = self.crowdmap.get_all_posts_for("Joe")
 		self.assertEquals([],posts)
+	
+	def test_get_all_posts_for_name(self):
+		posts = self.crowdmap.get_all_posts_for("Joe")
+		self.assertEquals(["I met Or A. at Chabad house Bangkok","We found Or A. R.I.P at Langtang valley"],posts)
 		
 		
 if __name__ == '__main__':
